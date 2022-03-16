681d6c9294a918df3282a1df6513f3c0e9dee61f
EC2 Default User <ec2-user@ip-172-31-9-31.us-east-2.compute.internal>
2022-03-16 19:41:03 +0000

    clean up added

--- database_persistence.rb
+++ database_persistence.rb
@@ -2,8 +2,16 @@ require "pg"
 
 class DatabasePersistence
   def initialize(logger)
-    @db = PG.connect(dbname: "todos")
+    @db = if Sinatra::Base.production?
+            PG.connect(ENV['DATABASE_URL'])
+          else
+            PG.connect(dbname: "todos")
+          end
     @logger = logger
+  end
+  
+  def disconnect
+    @db.close  
   end
   
   def query(statement, *params)
